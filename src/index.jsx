15351a786d08794e5cecc6845f21b587a4c8f6ae
Rodrigo Mendez <rodrigo.mendez@wgu.edu>
2023-08-22 15:04:20 -0600

    testing

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,11 @@ import {
   subscribe,
   initialize,
 } from '@edx/frontend-platform';
+
+
+
+
+
 import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
 import ReactDOM from 'react-dom';
 
