b619e1b15f833aa1a403afda47c1e24fb94f31af
Rodrigo Mendez <rodrigo.mendez@wgu.edu>
2023-08-24 15:32:35 -0600

    Added workaround to allow using npm link when developing both frontend-app-learning and this package

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,24 @@
-const { createConfig } = require("@edx/frontend-build");
+const { createConfig } = require('@edx/frontend-build');
+const path = require('path');
 
-module.exports = createConfig("webpack-dev-server", {
+module.exports = createConfig('webpack-dev-server', {
   devServer: {
-    allowedHosts: ["apps.local.overhang.io"],
+    allowedHosts: ['apps.local.overhang.io'],
+  },
+  resolve: {
+    /*
+      Make sure webpack picks only one version for each dependency that also exists in frontend-app-learning.
+      Useful when developing with npm link.
+    */
+    alias: {
+      '@edx/frontend-platform': path.resolve('./node_modules/@edx/frontend-platform'),
+      '@edx/paragon': path.resolve('./node_modules/@edx/paragon'),
+      'prop-types': path.resolve('./node_modules/prop-types'),
+      react: path.resolve('./node_modules/react'),
+      'react-dom': path.resolve('./node_modules/react-dom'),
+      'react-redux': path.resolve('./node_modules/react-redux'),
+      'react-router-dom': path.resolve('./node_modules/react-router-dom'),
+      redux: path.resolve('./node_modules/redux'),
+    },
   },
 });
