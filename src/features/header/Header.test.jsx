7fd580e7c469bc46495906a332585e0657d49d58
Rodrigo Mendez <rodrigo.mendez@wgu.edu>
2023-08-29 12:27:03 -0600

    Fix tests

--- src/features/header/Header.test.jsx
+++ src/features/header/Header.test.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 
 // Local Components
 import DesktopHeader from './DesktopHeader';
 import MobileHeader from './MobileHeader';
+import initializeStore from '../../app/store';
 
 describe('DesktopHeader', () => {
   it('displays course data', () => {
@@ -20,13 +22,21 @@ describe('DesktopHeader', () => {
 });
 
 describe('MobileHeader', () => {
+  let store;
+
+  beforeEach(() => {
+    store = initializeStore();
+  });
+
   it('displays course data', () => {
     const courseData = {
       courseNumber: 'course-number',
       courseTitle: 'course-title',
     };
 
-    render(<MobileHeader {...courseData} />);
+    render(
+      <Provider store={store}><MobileHeader {...courseData} /></Provider>,
+    );
 
     expect(screen.getByText(courseData.courseNumber, { exact: false }));
     expect(screen.getByText(courseData.courseTitle, { exact: false }));
