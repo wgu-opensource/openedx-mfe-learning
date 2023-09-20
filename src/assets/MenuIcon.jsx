df2e1163123836c171bf35be3ca829805bf2c5a9
AngelRicardezWGU <132600552+AngelRicardezWGU@users.noreply.github.com>
2023-09-19 20:30:35 -0600

    refactor(DEX-4083): header was refactored deleting unnecessary files … (#23)
    * refactor(DEX-4083): header was refactored deleting unnecessary files keeping only one header and styling the header to fit the design
    
    * refactor(DEX-4083): changing logo width

---
+++ src/assets/MenuIcon.jsx
@@ -0,0 +1,15 @@
+const MenuIcon = (props) => (
+  <svg
+    width="12px"
+    height="12px"
+    viewBox="0 0 16 16"
+    version="1.0"
+    {...props}
+  >
+    <rect fill="white" x="1" y="2.5" width="10" height="1" />
+    <rect fill="white" x="1" y="5.5" width="10" height="1" />
+    <rect fill="white" x="1" y="8.5" width="10" height="1" />
+  </svg>
+);
+
+export default MenuIcon;
--- src/features/header/Header.test.jsx
+++ src/features/header/Header.test.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { Factory } from 'rosie';
-import { Provider } from 'react-redux';
 import { breakpoints } from '@edx/paragon';
 import { Context as ResponsiveContext } from 'react-responsive';
 
 // Local Components
-import DesktopHeader from './DesktopHeader';
-import MobileHeader from './MobileHeader';
-import initializeStore from '../../app/store';
 import Header from './Header';
 import { initializeTestStore, render, screen } from '../../setupTest';
 
@@ -27,41 +23,5 @@ describe('Header', () => {
     );
     expect(screen.getByText(courseHomeMetadata.number, { exact: false }));
     expect(screen.getByText(courseHomeMetadata.title, { exact: false }));
-  });
-});
-
-describe('DesktopHeader', () => {
-  it('displays course data', () => {
-    const courseData = {
-      courseNumber: 'course-number',
-      courseTitle: 'course-title',
-    };
-
-    render(<DesktopHeader {...courseData} />);
-
-    expect(screen.getByText(courseData.courseNumber, { exact: false }));
-    expect(screen.getByText(courseData.courseTitle, { exact: false }));
-  });
-});
-
-describe('MobileHeader', () => {
-  let store;
-
-  beforeEach(() => {
-    store = initializeStore();
-  });
-
-  it('displays course data', () => {
-    const courseData = {
-      courseNumber: 'course-number',
-      courseTitle: 'course-title',
-    };
-
-    render(
-      <Provider store={store}><MobileHeader {...courseData} /></Provider>,
-    );
-
-    expect(screen.getByText(courseData.courseNumber, { exact: false }));
-    expect(screen.getByText(courseData.courseTitle, { exact: false }));
   });
 });
