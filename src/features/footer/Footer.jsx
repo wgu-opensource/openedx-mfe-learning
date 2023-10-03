51b18286f76dacf08254e4f5fb6e49af7f6d2767
Rodrigo Mendez <rodrigo.mendez@wgu.edu>
2023-10-03 15:46:49 -0600

    Implemented alternative solution

--- src/features/footer/Footer.jsx
+++ src/features/footer/Footer.jsx
@@ -53,54 +53,57 @@ const Footer = ({ className }) => {
       role="contentinfo"
       className={classNames('footer', className)}
     >
-      <div className="footer-section-container">
-        <img
-          src={logo || config.LOGO_TRADEMARK_URL}
-          alt={logoAltText}
-        />
-        <a
-          className="d-block branded-link aria-tooltip-link"
-          href={adaUrl}
-          target="_blank"
-          rel="noreferrer"
-          aria-label="ADA Accommodation (opens in new tab)"
-          onClick={externalLinkClickHandler}
-        >
-          ADA Accommodation
-          <span className="aria-tooltip">Opens in new tab</span>
-        </a>
-      </div>
-      <div className="footer-section-container footer-bottom-container">
-        <div className="footer-copy">
-          <span>
-            {COPY_SYMBOL}
-            {' '}
-            {copyRight}
-          </span>
+      <div className="footer-background">
+        <div className="footer-section-container">
+          <img
+            src={logo || config.LOGO_TRADEMARK_URL}
+            alt={logoAltText}
+          />
+          <a
+            className="d-block branded-link aria-tooltip-link"
+            href={adaUrl}
+            target="_blank"
+            rel="noreferrer"
+            aria-label="ADA Accommodation (opens in new tab)"
+            onClick={externalLinkClickHandler}
+          >
+            ADA Accommodation
+            <span className="aria-tooltip">Opens in new tab</span>
+          </a>
         </div>
-        <div className="footer-links-container">
-          {links.map((link, index) => (
-            <>
-              <a
-                className="info-link aria-tooltip-link"
-                href={link.url}
-                target="_blank"
-                rel="noreferrer"
-                aria-label={`${link.label} (opens in new tab)`}
-                onClick={externalLinkClickHandler}
-              >
-                {link.label}
-                <span className="aria-tooltip">Opens in new tab</span>
-              </a>
-              {links.length !== (index + 1) && (
-              <span className="link-divider">
-                |
-              </span>
-              )}
-            </>
-          ))}
+        <div className="footer-section-container footer-bottom-container">
+          <div className="footer-copy">
+            <span>
+              {COPY_SYMBOL}
+              {' '}
+              {copyRight}
+            </span>
+          </div>
+          <div className="footer-links-container">
+            {links.map((link, index) => (
+              <>
+                <a
+                  className="info-link aria-tooltip-link"
+                  href={link.url}
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label={`${link.label} (opens in new tab)`}
+                  onClick={externalLinkClickHandler}
+                >
+                  {link.label}
+                  <span className="aria-tooltip">Opens in new tab</span>
+                </a>
+                {links.length !== (index + 1) && (
+                <span className="link-divider">
+                  |
+                </span>
+                )}
+              </>
+            ))}
+          </div>
         </div>
       </div>
+
     </footer>
   );
 };
