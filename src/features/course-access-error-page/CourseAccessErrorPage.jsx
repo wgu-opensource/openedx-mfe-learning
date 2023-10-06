8512d641d1c5a8876b440cbf08c1a0ca7217f607
Rodrigo Mendez <rodrigo.mendez@wgu.edu>
2023-10-06 10:26:53 -0600

    Added config vars for access-denied screen

--- src/features/course-access-error-page/CourseAccessErrorPage.jsx
+++ src/features/course-access-error-page/CourseAccessErrorPage.jsx
@@ -1,8 +1,19 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { ensureConfig, getConfig } from '@edx/frontend-platform/config';
 import { setLayoutHasSidebar } from '../course-view/data/slice';
 
+ensureConfig([
+  'ACCESS_DENIED_PAGE_INSTRUCTIONS_LINK',
+  'ACCESS_DENIED_PAGE_STUDENT_PORTAL_LINK',
+  'ACCESS_DENIED_PAGE_STUDENT_PORTAL_LINK_TEXT',
+], 'CoursePlayer component');
+
 const CourseAccessErrorPage = () => {
+  const instructionsLink = getConfig().ACCESS_DENIED_PAGE_INSTRUCTIONS_LINK;
+  const studentPortalLink = getConfig().ACCESS_DENIED_PAGE_STUDENT_PORTAL_LINK;
+  const studentPortalText = getConfig().ACCESS_DENIED_PAGE_STUDENT_PORTAL_LINK_TEXT;
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -17,14 +28,14 @@ const CourseAccessErrorPage = () => {
       <p>
         It may be due to a browser issue.
         Please follow these&nbsp;
-        <a href="https://example.com/" target="_blank" rel="noreferrer">
+        <a href={instructionsLink} target="_blank" rel="noreferrer">
           instructions to clear your cookies
         </a>.
       </p>
       <p>
         Then try accessing the course again through the&nbsp;
-        <a href="https://example.com/" target="_blank" rel="noreferrer">
-          Student Portal
+        <a href={studentPortalLink} target="_blank" rel="noreferrer">
+          {studentPortalText}
         </a>.
       </p>
     </div>
--- src/index.jsx
+++ src/index.jsx
@@ -84,6 +84,9 @@ initialize({
         TWITTER_URL: process.env.TWITTER_URL || null,
         LEGACY_THEME_NAME: process.env.LEGACY_THEME_NAME || null,
         DISABLE_APP_HEADER: process.env.DISABLE_APP_HEADER === 'true' || null,
+        ACCESS_DENIED_PAGE_INSTRUCTIONS_LINK: process.env.ACCESS_DENIED_PAGE_INSTRUCTIONS_LINK || '',
+        ACCESS_DENIED_PAGE_STUDENT_PORTAL_LINK: process.env.ACCESS_DENIED_PAGE_STUDENT_PORTAL_LINK || '',
+        ACCESS_DENIED_PAGE_STUDENT_PORTAL_LINK_TEXT: process.env.ACCESS_DENIED_PAGE_STUDENT_PORTAL_LINK_TEXT || 'Student Portal',
       }, 'LearnerAppConfig');
     },
   },
